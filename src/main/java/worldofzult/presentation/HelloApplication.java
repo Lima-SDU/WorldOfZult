3c10c07dbf643792b342c0a0bb8d7ac4d2fbd3cd
MJP <madsjep23@gmail.com>
2023-11-22 12:23:19 +0100

    Test af ImageView En lille ændring til demo-programmet, der tillader en at ændre billede ved hjælp af en knap.

--- src/main/java/worldofzult/presentation/HelloApplication.java
+++ src/main/java/worldofzult/presentation/HelloApplication.java
@@ -1,6 +1,8 @@
 package worldofzult.presentation;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -11,7 +13,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("guiwoz.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/worldofzult/presentation/HelloController.java
+++ src/main/java/worldofzult/presentation/HelloController.java
@@ -1,28 +1,56 @@
 package worldofzult.presentation;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
+    @FXML
+    public ImageView imageDisplay;
+
     @FXML
     private Label welcomeText;
 
     @FXML
     public Button userDataButton;
 
+    Image image1 = new Image("file:src/main/resources/worldofzult/presentation/images/cat-behavior-social.jpg");
+    Image image2 = new Image("file:src/main/resources/worldofzult/presentation/images/vizsla-running.jpg");
+
+
     @FXML
     public void initialize() {
         userDataButton.setUserData("Gå vest");
-    }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        imageDisplay.setImage(image1);
+        imageDisplay.setCache(true);
     }
 
     @FXML
     protected void onUserButtonClick() {
         System.out.println(userDataButton.getUserData());
+    }
+
+    @FXML
+    public void onImageButtonClick(ActionEvent actionEvent) {
+        if (imageDisplay.getImage() == image2) {
+            imageDisplay.setImage(image1);
+        } else {
+            imageDisplay.setImage(image2);
+        }
+    }
+}
+
+class ImageFile {
+    SimpleStringProperty filepath;
+    Image image;
+
+    ImageFile(String filepath) {
+        this.filepath = new SimpleStringProperty(filepath);
+        image = new Image(String.valueOf(this.filepath));
     }
 }
