a24ee829f280ba48cf24b28d3046bb8f48495c49
cgrevy <chgre23@student.sdu.dk>
2023-11-25 20:57:06 +0100

    Begrænset stage
    Så den ikke kan ændre størrelse

--- src/main/java/worldofzult/presentation/HelloApplication.java
+++ src/main/java/worldofzult/presentation/HelloApplication.java
@@ -12,10 +12,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("guiny.fxml"));
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("guiwoz.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
