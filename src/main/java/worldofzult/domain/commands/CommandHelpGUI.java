1c28402a1ef8c424127afd1a447a71b072985b96
Emil Kjær Fuglsang <emilkf95@gmail.com>
2023-12-06 15:40:03 +0100

    Fixed grammar in helpCommangGUI

--- src/main/java/worldofzult/domain/commands/CommandHelpGUI.java
+++ src/main/java/worldofzult/domain/commands/CommandHelpGUI.java
@@ -5,8 +5,9 @@ import worldofzult.domain.session.Context;
 public class CommandHelpGUI extends BaseCommand implements Command{
     @Override
     public String execute(Context context, String command, String[] parameters) {
-        return "\nFind redskaber på jorden, klik på dem for at samle dem op og find den gruppe som mangler lige det redskabet. " +
-                "Når du er har fundet den sultne gruppe så klik på redskabet i dit inventar og gi' redskabet til gruppen for at " +
+        return "\nTal med folk og undersøg hvilke udfordringer de har. " +
+                "Find redskaber på jorden, klik på dem for at samle dem op og find den gruppe som mangler lige det redskab. " +
+                "Når du har fundet den rette, så klik på redskabet i dit inventar og giv redskabet til dem for at " +
                 "løse deres problem.\n" + "\n";
     }
 }
