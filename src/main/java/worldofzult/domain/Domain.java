ade0bee9c28f30345ec3ff2a137cf81263a803b5
MJP <madsjep23@gmail.com>
2023-12-06 15:26:22 +0100

    Added comments to Domain

--- src/main/java/worldofzult/domain/Domain.java
+++ src/main/java/worldofzult/domain/Domain.java
@@ -14,31 +14,43 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class Domain {
-    private Game game;
-    private Context context;
-    private Registry registry;
-    private Quiz quiz;
+    private Game game; // Contains current game
+    private Context context; // Session-information
+    private Registry registry; // Registry of commands
+    private Quiz quiz; // Quiz
 
+    // Constructor
     public Domain() {
+        // Make new game
        game = new Game();
        game.initRegistry();
+
+       // Get context and registry for easier access than accessing through game
        context = game.getContext();
        registry = game.getRegistry();
+
+       // Create new player and set default name
        Player player = new Player("Spiller");
        context.setPlayer(player);
+
+       // Initialize quiz
        this.quiz = new Quiz();
        quiz.initQuiz();
     }
 
+    // Run command through registry
     public String runCommand(String command) {
         return registry.dispatch(command);
     }
 
+    // Get name of the current room
     public String getCurrent() {
         return context.getCurrent().getName();
     }
 
+    // Check status of the group in a room
     public boolean checkGroupStatus() {
+        // Return status if there is a group
         if (context.getCurrent().getGroup() != null) {
             return this.context.getCurrent().getGroup().isHungry();
         } else {
@@ -46,9 +58,13 @@ public class Domain {
         }
     }
 
+    // Get current exits / edges of the room
     public boolean[] getCurrentExits() {
-        boolean[] result = new boolean[4];
-        Map<String, Node> edges = context.getCurrent().getEdges();
+        boolean[] result = new boolean[4]; // Boolean array containing each cardinal direction
+
+        Map<String, Node> edges = context.getCurrent().getEdges(); // Get edges
+
+        // Go through each edge of the room and set the respective boolean to true
         for (String key : edges.keySet()) {
             switch (key) {
                 case "nord" -> result[0] = true;
@@ -57,37 +73,53 @@ public class Domain {
                 case "vest" -> result[3] = true;
             }
         }
-        return result;
+
+        return result; // Return array
     }
 
+    // Get a string containing the names of the items in a room
     public ArrayList<String> getItemsInRoom() {
+        // Get the rooms inventory of items
         ArrayList<Item> items = context.getCurrent().getInventory().getItems();
         ArrayList<String> itemsString = new ArrayList<String>();
+
+        // Get the name of each item and add it to an ArrayList of their item names as Strings
         for (Item item : items) {
             itemsString.add(item.getName());
         }
+
+        // Return ArrayList of the item names
         return itemsString;
     }
 
+    // Get a string containing the names of the items in the player's inventory
     public ArrayList<String> getItemsInInventory() {
+        // Get the rooms inventory of items
         ArrayList<Item> items = context.getPlayer().getInventory().getItems();
         ArrayList<String> itemsString = new ArrayList<String>();
+
+        // Get the name of each item and add it to an ArrayList of their item names as Strings
         for (Item item : items) {
             itemsString.add(item.getName());
         }
+
+        // Return ArrayList of the item names
         return itemsString;
     }
 
+    // Get information of item at specific index in inventory
     public String getItemInformation(int index) {
+        // Get a list of the items
         ArrayList<Item> items = context.getPlayer().getInventory().getItems();
 
-        if (index > items.size() - 1) {
+        if (index > items.size() - 1) { // If there is no item in the specified index, return empty string
             return "";
         } else {
-            return items.get(index).getDescription();
+            return items.get(index).getDescription(); // Get description of item
         }
     }
 
+    // Returns if the game is done
     public boolean checkIsDone() {
         return context.isDone();
     }
